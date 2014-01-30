15d6c87bc5d259fd46bb2ef4603842c57df52ed9
boniface <20Mataba04>
2014-01-30 14:38:05 +0200

    Updated Case

--- src/test/java/zm/hashcode/mshengu/test/services/SiteVisitDayServiceLogTaskTest.java
+++ src/test/java/zm/hashcode/mshengu/test/services/SiteVisitDayServiceLogTaskTest.java
@@ -59,7 +59,6 @@ public class SiteVisitDayServiceLogTaskTest extends AppTest {
         System.out.println("Count " + size);
         System.out.println("\n\n================= DAY [ " + dtfwh.getDayOfWeekTodayStr() + " ] - VISIT DATE : " + calendar.getTime());
         for (Site site : sitesList) {
-
             count++;
             System.out.println("\n\n --- Site No" + count + "/" + size + "---");
             siteServiceScheduleLogsService.createSiteServiceLog(site, dtfwh.getDateToday());
@@ -68,44 +67,5 @@ public class SiteVisitDayServiceLogTaskTest extends AppTest {
 //        logSheduledSiteServices.createTodaysSiteServicesLogs2(calendar.getTime());
     }
 
-//    @Test
-    public void updateLogs(Date date) {
-//        logSheduledSiteServices = ctx.getBean(LogSheduledSiteServices.class);
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTime(startDate);
-//        setTodaysDate(calendar.getTime());
-//        logSheduledSiteServices = ctx.getBean(LogSheduledSiteServices.class);
-//        logSheduledSiteServices.updateOpensSiteServicesLogs(date);
-    }
 
-//    @Test
-    public void closeLogs(Date date) {
-//        logSheduledSiteServices = ctx.getBean(LogSheduledSiteServices.class);
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTime(startDate);
-//        setTodaysDate(calendar.getTime());
-//        logSheduledSiteServices = ctx.getBean(LogSheduledSiteServices.class);
-//        logSheduledSiteServices.closeOutdatedSiteServicesLogs(date);
-    }
-
-//    @Test
-    public void createLogsDirectly() {
-//        createSiteServiceLogsService = ctx.getBean(CreateSiteServiceLogsService.class);
-//        siteService = ctx.getBean(SiteService.class);
-//        Calendar calendar = Calendar.getInstance();
-//        calendar.setTime(startDate);
-//        setTodaysDate(calendar.getTime());
-//
-//        List<Site> sitesList = siteService.findAllWithVisitToday(calendar.getTime());
-//        int count = 0;
-//        int size = sitesList.size();
-//        System.out.println("\n\n================= TEST VISIT DATE : " + calendar.getTime());
-//        for (Site site : sitesList) {
-//            count++;
-//            System.out.println("\n\n --- Site No" + count + "/" + size + "---");
-//            System.out.println("Site :" + site.getName());
-//
-//            createSiteServiceLogsService.createSiteServiceLog(site, calendar.getTime());
-//        }
-    }
 }
