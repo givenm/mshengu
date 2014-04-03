82cbf65f59ab56b6b1e410c072df6d1c7a865cc6
= <=>
2014-04-03 23:51:35 +0200

    FIxes for siteServiceLogs serviceBy and siteUnitServiceLog service by plus test cases

--- src/test/java/zm/hashcode/mshengu/test/services/RemoveSiteServiceLogsTest.java
+++ src/test/java/zm/hashcode/mshengu/test/services/RemoveSiteServiceLogsTest.java
@@ -25,9 +25,7 @@ import static zm.hashcode.mshengu.test.AppTest.ctx;
  */
 public class RemoveSiteServiceLogsTest extends AppTest {
 
-    @Autowired
-    private ServiceUnit serviceUnit;
-    @Autowired
+     @Autowired
     private SiteService siteService;
     @Autowired
     private RequestService requestService;
@@ -37,40 +35,50 @@ public class RemoveSiteServiceLogsTest extends AppTest {
 
         siteService = ctx.getBean(SiteService.class);
         List<Site> siteList = siteService.findAll();
-
+       int totalBefore = 0;
+       int totalAfter = 0;
+       
+        System.out.println("\n\nsite count " + siteList.size());
         for (Site site : siteList) {
+            totalBefore += site.getSiteServiceLog().size();
             Set<SiteServiceLog> siteServiceLogList = new HashSet<>();
             Site newSite = new Site.Builder(site.getName())
                     .site(site)
                     .siteServiceLog(siteServiceLogList)
                     .build();
 
-            siteService.merge(newSite);
+//            siteService.merge(newSite);
+            
+            totalAfter += newSite.getSiteServiceLog().size();
 
         }
+        System.out.println("\nTotal Site Sevice logs before " +  totalBefore);
+        System.out.println("\n");
+        System.out.println("\nTotal Site Sevice logs after " + totalAfter);
     }
 
-    @Test
+//    @Test
     public void updatePurchaseOrderTotal() {
         requestService = ctx.getBean(RequestService.class);
 
-//        Request request = requestService.findById("530241fb334efdd25d91cfc9");
+        Request request = requestService.findById("530241fb334efdd25d91cfc9");
         
-        Request request = requestService.findByOrderNumber("MSH_PO-000025");
+//        Request request = requestService.findByOrderNumber("MSH_PO-000025");
 
-        System.out.println("Before update");
-        System.out.print(" --> Request Number " + request.getOrderNumber());
-        System.out.print(" --> Request Total " + request.getTotal());
+        System.out.println("\n\nBefore update");
+        System.out.println(" --> Request Number " + request.getOrderNumber());
+        System.out.println(" --> Request Total " + request.getTotal());
         Request newrequest = new Request.Builder(request.getPerson())
                 .request(request)
                 .total(BigDecimal.ZERO)
                 .build();
 
-//        requestService.merge(newrequest);
-        Request request2 = requestService.findByOrderNumber("MSH_PO-000025");
+        requestService.merge(newrequest);
+//        Request request2 = requestService.findByOrderNumber("MSH_PO-000025");
+        Request request2 = requestService.findById("530241fb334efdd25d91cfc9");
         System.out.println("\n\n-------------------------------\n After Updateate");
-        System.out.print(" --> Request Number " + request2.getOrderNumber());
-        System.out.print(" --> Request Total " + request2.getTotal());
+        System.out.println(" --> Request Number " + request2.getOrderNumber());
+        System.out.println(" --> Request Total " + request2.getTotal());
 
     }
 
